8718cebbd772852ac38f1bdc75a399b525cff1a7
fredRan24 <86071505+fredRan24@users.noreply.github.com>
2021-12-21 16:31:46 +0000

    Added Function shells from planning doc
    Added Class for Wilber 3 Directories
    
    Added Class for Wilbur 3 DIR Converter
    
    Added appropriate function shells for implementation.

--- CrustWobble/Source/Quake.cpp
+++ CrustWobble/Source/Quake.cpp
@@ -9,3 +9,54 @@
 */
 
 #include "Quake.h"
+
+/*
+==============================================================================
+                                Wilber3DIR
+==============================================================================
+*/
+Wilber3DIR::Wilber3DIR() {
+    
+}
+
+Wilber3DIR::~Wilber3DIR() {
+    
+}
+
+void Wilber3DIR::checkDIR() {
+    
+}
+
+void Wilber3DIR::getNumFiles() {
+    
+}
+
+void Wilber3DIR::checkFiletype() {
+    
+}
+
+void Wilber3DIR::locateStationFile() {
+    
+}
+
+void Wilber3DIR::readStationFile() {
+    
+}
+/*
+==============================================================================
+                                W3DIRConverter
+==============================================================================
+*/
+
+W3DIRConverter::W3DIRConverter() {
+    
+}
+
+W3DIRConverter::~W3DIRConverter() {
+    
+}
+
+void W3DIRConverter::convertW3DIR(Wilber3DIR directory) {
+    
+}
+                                                        
--- CrustWobble/Source/Quake.h
+++ CrustWobble/Source/Quake.h
@@ -7,5 +7,40 @@
 
   ==============================================================================
 */
+#include <JuceHeader.h>
+
+using namespace juce;
 
 #pragma once
+class Wilber3DIR
+{
+public:
+    Wilber3DIR();
+    ~Wilber3DIR();
+    
+    void checkDIR();
+    
+    void getNumFiles();
+    void checkFiletype();
+    void locateStationFile();
+    
+    void readStationFile();
+    
+private:
+    String W3DIRName = "";
+    StringArray files;
+    String stationFilePath = "";
+};
+
+class W3DIRConverter
+{
+public:
+    W3DIRConverter();
+    ~W3DIRConverter();
+    
+    void convertW3DIR(Wilber3DIR directory);
+    
+private:
+    
+};
+
