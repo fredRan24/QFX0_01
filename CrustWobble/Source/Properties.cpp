f7ca8023cad6175a832bfd8740945d5c5a13bd89
fredRan24 <86071505+fredRan24@users.noreply.github.com>
2023-01-01 23:43:42 +0000

    Added Properties Class
    Added Propertied class to be used by Yggdrasil ValueTrees

---
+++ CrustWobble/Source/Properties.cpp
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    Properties.cpp
+    Created: 28 Dec 2022 2:09:58pm
+    Author:  Alfie Ransome
+
+  ==============================================================================
+*/
+
+#include "Properties.h"
+#include <JuceHeader.h>
+
+Properties::Properties() {
+    
+    
+}
+
+Properties::~Properties() {
+    
+    
+}
+
+const Identifier Properties::ROOT = "ROOT";
+
+const Identifier Properties::VIEWS = "VIEWS";
+const Identifier Properties::FILEBAR = "FILEBAR";
+const Identifier Properties::LISTWINDOW = "LISTWINDOW";
+const Identifier Properties::VISUALISER = "VISUALISER";
+const Identifier Properties::CONTROLS = "CONTROLS";
+
+const Identifier Properties::EVENTROOT = "EVENTROOT";
